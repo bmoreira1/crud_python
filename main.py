56f5f0ef9b8d317eb1787b168a8905506fda9228
Bruno Barbosa Moreira <bbmoreira.195@gmail.com>
2025-06-12 14:20:17 -0300

    ajuste final para a entrega do trabalho da matéria de Desenvolvimento Rápido de aplicações em Python

--- main.py
+++ main.py
@@ -15,15 +15,11 @@ co7 = "#ef5350"   # vermelha
 co8 = "#263238"   # + verde
 co9 = "#e9edf5"   # sky blue
 
-
-
 janela = tk.Tk()
 janela.title("")
 janela.geometry('1483x653')
 janela.configure(background=co9)
 janela.resizable(width=False, height=False)
-
-
 
 frame_cima = tk.Frame(janela, width=310, height=50, bg=co2, relief='flat')
 frame_cima.grid(row=0, column=0)
@@ -33,7 +29,6 @@ frame_baixo.grid(row=1, column=0, padx=0, pady=0, sticky=tk.NSEW)
 
 frame_direita = tk.Frame(janela, width=588, height=403, bg=co1, relief='flat')
 frame_direita.grid(row=0, column=1, rowspan=2, padx=1, sticky=tk.NSEW)
-
 
 app_nome = tk.Label(frame_cima, text='Dados Empresas', anchor=tk.NW, font=('Ivy 13 bold'), bg=co2, fg=co1, relief='flat')
 app_nome.place(x=10, y=20)
@@ -71,12 +66,21 @@ def inserir():
         widget.destroy()
     mostrar()
 
-
 def atualizar():
     try:
         treev_dados = tree.focus()
+
+        if not treev_dados:
+            messagebox.showerror("Nenhum item selecionado.")
+            return
+
         treev_dicionario = tree.item(treev_dados)
         tree_lista = treev_dicionario['values']
+
+
+        if len(tree_lista) < 9:
+            messagebox.showerror("Lista de valores muito curta para preencher todos os campos de entrada.")
+            return
 
         valor_id = tree_lista[0]
 
@@ -92,7 +96,6 @@ def atualizar():
         entry_cnpj.insert(0, tree_lista[1])
         entry_razao_social.insert(0, tree_lista[2])
         entry_qualificacao_responsavel.insert(0, tree_lista[3])
-
         entry_capital_social.insert(0, tree_lista[4])
         entry_porte_empresa.insert(0, tree_lista[5])
         entry_ente_federativo.insert(0, tree_lista[6])
@@ -103,11 +106,7 @@ def atualizar():
             cnpj = entry_cnpj.get()
             razao_social = entry_razao_social.get()
             qualificacao_responsavel = entry_qualificacao_responsavel.get()
-            print(f"CNPJ: {cnpj}")
-            print(f"Razão Social: {razao_social}")
-            print(f"Qualificação Responsável: {qualificacao_responsavel}")
             capital_social = entry_capital_social.get()
-            print(f"capital: {capital_social}")
             porte_empresa = entry_porte_empresa.get()
             ente_federativo = entry_ente_federativo.get()
             descricao_natureza_juridica = entry_descricao_natureza_juridica.get()
@@ -134,11 +133,11 @@ def atualizar():
                 widget.destroy()
 
             mostrar()
-        botao_Confirmar = tk.Button(frame_baixo,command=update, text='Confirmar', width=10, font=("ivi 9 bold"), bg=co2, fg=co1, relief='raised', overrelief='ridge')
-        botao_Confirmar.place(x=110, y=570)
+        botao_Confirmar = tk.Button(frame_baixo,command=update, text='Atualizar', width=10, font=("ivi 9 bold"), bg=co2, fg=co1, relief='raised', overrelief='ridge')
+        botao_Confirmar.place(x=110, y=510)
 
-    except IndexError:
-        messagebox.showerror('Error', 'seleciona um dos dados na tabela')
+    except Exception as e:
+        messagebox.showerror(f"Ocorreu um erro ao atualizar os campos: {e}")
 
 def deletar():
     try:
@@ -158,7 +157,6 @@ def deletar():
 
     except IndexError:
         messagebox.showerror('Error', 'seleciona um dos dados na tabela')
-
 
 label_cnpj = tk.Label(frame_baixo, text='CNPJ:', anchor=tk.NW, font=('Ivy 13 bold'), bg=co1, fg=co4, relief='flat')
 label_cnpj.place(x=10, y=10)
@@ -186,6 +184,7 @@ label_capital_social.place(x=10, y=190)
 
 entry_capital_social = tk.Entry(frame_baixo, width=45, justify='left',  relief='solid')
 entry_capital_social.place(x=15, y=220)
+
 
 label_porte_empresa = tk.Label(frame_baixo, text='Porte da Empresa:', anchor=tk.NW, font=('Ivy 13 bold'), bg=co1, fg=co4, relief='flat')
 label_porte_empresa.place(x=10, y=250)
@@ -215,14 +214,8 @@ entry_descricao_porte_empresa = tk.Entry(frame_baixo, width=45, justify='left', 
 entry_descricao_porte_empresa.place(x=15, y=460)
 
 
-
 botao_inserir = tk.Button(frame_baixo, command=inserir, text='Inserir', width=10, font=("ivi 9 bold"), bg=co6, fg=co1, relief='raised', overrelief='ridge')
 botao_inserir.place(x=15, y=510)
-
-
-botao_atualizar = tk.Button(frame_baixo, command=atualizar, text='Atualizar', width=10, font=("ivi 9 bold"), bg=co2, fg=co1, relief='raised', overrelief='ridge')
-botao_atualizar.place(x=110, y=510)
-
 
 botao_deletar = tk.Button(frame_baixo, command=deletar, text='Deletar', width=10, font=("ivi 9 bold"), bg=co7, fg=co1, relief='raised', overrelief='ridge')
 botao_deletar.place(x=205, y=510)
@@ -235,12 +228,10 @@ def mostrar():
 
     tabela_head = ['ID','CNPJ', 'RAZÃO SOCIAL','QUALIFICAÇÃO RESPONSÁVEL', 'CAPITAL SOCIAL', 'PORTE DA EMPRESA', 'ENTE FEDERATIVO', 'DESCRIÇÃO NATUREZA JURÍDICA','DESCRIÇÃO PORTE DA EMPRESA']
 
-
     tree = ttk.Treeview(frame_direita, selectmode="extended", columns=tabela_head, show="headings")
 
     vsb = ttk.Scrollbar(frame_direita, orient="vertical", command=tree.yview)
-
-    hsb = ttk.Scrollbar( frame_direita, orient="horizontal", command=tree.xview)
+    hsb = ttk.Scrollbar(frame_direita, orient="horizontal", command=tree.xview)
 
     tree.configure(yscrollcommand=vsb.set, xscrollcommand=hsb.set)
     tree.grid(column=0, row=0, sticky='nsew')
@@ -249,7 +240,6 @@ def mostrar():
 
     frame_direita.grid_rowconfigure(0, weight=12)
 
-
     hd=["center","center","center","center","center","center","center","center","center"]
     h=[40,130,130,130,130,130,130, 180, 180]
     n=0
@@ -257,12 +247,12 @@ def mostrar():
     for col in tabela_head:
         tree.heading(col, text=col.title(), anchor=tk.CENTER)
         tree.column(col, width=h[n],anchor=hd[n])
-
         n+=1
 
     for item in lista:
         tree.insert('', 'end', values=item)
 
+    tree.bind('<<TreeviewSelect>>', lambda event: atualizar())
 
 mostrar()
 
